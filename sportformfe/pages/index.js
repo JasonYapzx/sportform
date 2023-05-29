076fe91ff903158d545deaba3be382ccfb53eda9
JasonYapzx <jasonyapzx@gmail.com>
2023-05-30 00:47:41 +0800

    feat: README

--- sportformfe/pages/index.js
+++ sportformfe/pages/index.js
@@ -54,45 +54,6 @@ export default function Home() {
             ></img>
           </div>
         </div>
-        {/* <section className="mx-auto">
-          <div className="container px-5 mx-auto lg:px-24 ">
-            <div className="flex flex-col w-full mb-4 text-left lg:text-center">
-              <h1 className="mb-8 text-2xl Avenir font-semibold text-black">
-                Trusted by top-tier product companies
-              </h1>
-            </div>
-            <div className="grid grid-cols-2 gap-16 mb-16 text-center lg:grid-cols-4">
-              <div className="flex items-center justify-center">
-                <img
-                  src="/images/Google-Logo.webp"
-                  alt="Google Logo"
-                  className="block object-contain h-16 greyC"
-                ></img>
-              </div>
-              <div className="flex items-center justify-center">
-                <img
-                  src="/images/Shopify-Logo.svg"
-                  alt="Shopify Logo"
-                  className="block object-contain h-16 greyC"
-                ></img>
-              </div>
-              <div className="flex items-center justify-center">
-                <img
-                  src="/images/Cloudflare-Logo.svg"
-                  alt="Cloudflare Logo"
-                  className="block object-contain h-16 greyC"
-                ></img>
-              </div>
-              <div className="flex items-center justify-center">
-                <img
-                  src="/images/PayPal-Logo.png"
-                  alt="Paypal Logo"
-                  className="block object-contain h-16 greyC"
-                ></img>
-              </div>
-            </div>
-          </div>
-        </section> */}
         
         <section className="relative">
           <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
